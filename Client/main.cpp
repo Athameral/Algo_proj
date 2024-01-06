a7f0146a81ee208e390afc63420187bed4917faf
Athameral <Athameral@outlook.com>
2024-01-06 17:00:06 +0800

    收发已经没问题了。

---
+++ Client/main.cpp
@@ -0,0 +1,49 @@
+/*****************************************************************//**
+ * \file   main.cpp
+ * \brief  客户端
+ * 
+ * \author sunny
+ * \date   January 2024
+ *********************************************************************/
+
+#include <iostream>
+#include <array>
+#include <random>
+#include "mysock.hpp"
+
+std::array<int, 2560> rawFloatData;
+
+int main()
+{
+	{
+		auto i = 0;
+		for (auto& e : rawFloatData)
+			e = i++;
+	}
+
+	MySocket_tcp socket;
+
+	// 连接到服务器
+	if (!socket.Connect("127.0.0.1", 8123))
+	{
+		return 1;
+	}
+
+	// 发送数据
+	if (!socket.Send(static_cast<const void*>(rawFloatData.data()), rawFloatData.size() * sizeof(int)))
+	{
+		std::cout << "finish" << std::endl;
+		return 1;
+	}
+
+	// 接收数据
+	/*std::string receivedData = socket.Receive();
+	if (receivedData.empty())
+	{
+		return 1;
+	}
+
+	std::cout << "Received: " << receivedData << std::endl;*/
+
+	return 0;
+}
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,7 +1,7 @@
 /*****************************************************************//**
  * \file   main.cpp
- * \brief  
- * 
+ * \brief  服务端
+ *
  * \author sunny
  * \date   January 2024
  *********************************************************************/
@@ -11,39 +11,30 @@
 #include <random>
 #include "mysock.hpp"
 
-std::array<int, 256000> rawFloatData;
+std::array<int, 2560> rawFloatData;
 
 int main()
 {
-    {
-        auto i = 0;
-        for (auto& e : rawFloatData)
-            e = i++;
-    }
+	MySocket_tcp socket;
 
-    MySocket_tcp socket;
+	// 开始监听
+	if (!socket.Listen_Accept("127.0.0.1", 8123))
+	{
+		return 1;
+	}
 
-    // 连接到服务器
-    if (!socket.Connect("127.0.0.1", 8123))
-    {
-        return 1;
-    }
+	// 接收数据
+	if (!socket.Receive(static_cast<void*>(rawFloatData.data()), rawFloatData.size() * sizeof(float)))
+	{
+		return 1;
+	}
+	std::cout << "finish" << std::endl;
 
-    // 发送数据
-    if (!socket.Send(static_cast<const void*>(rawFloatData.data()), rawFloatData.size() * sizeof(int)))
-    {
-        std::cout << "finish" << std::endl;
-        return 1;
-    }
+	for (auto i = rawFloatData.cbegin(); i < rawFloatData.cbegin() + 10; ++i)
+		std::cout << *i << std::endl;
 
-    // 接收数据
-    std::string receivedData = socket.Receive();
-    if (receivedData.empty())
-    {
-        return 1;
-    }
+	for (auto i = rawFloatData.cend() - 10; i < rawFloatData.cend(); ++i)
+		std::cout << *i << std::endl;
 
-    std::cout << "Received: " << receivedData << std::endl;
-
-    return 0;
+	return 0;
 }
